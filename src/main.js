6b82f0d9f70731c4ffe4b4628640f48e48f83188
unknown <diegomanuelcaca@gmail.com>
2025-04-03 14:08:47 -0500

    3 commit

--- src/main.js
+++ src/main.js
@@ -1,76 +1,20 @@
-import { createApp } from 'vue'
-import {createPinia} from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import App from './App.vue'
-import { Quasar, Notify} from 'quasar'
-import router from "./routes/routes.js"
-import '@quasar/extras/material-icons/material-icons.css'
-// Import Quasar css
-import 'quasar/src/css/index.sass'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import { Quasar, Notify } from "quasar";
+import router from "./routes/routes.js";
+import "quasar/src/css/index.sass";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:4000/";
 axios.defaults.baseURL = "https://backend-dji.onrender.com";
 
+const pinia = createPinia();
+const myApp = createApp(App);
 
-let requestsPending = 0;
-
-axios.interceptors.request.use((request) => {
-  requestsPending ++;
-
-if (requestsPending == 1) {
-  Swal.fire({
-    didOpen: () => {
-      Swal.showLoading();
-    },
-    allowOutsideClick: false,
-    allowEscakey: false,
-    allowEnterkey: false,
-  });
-}
-return request;
+myApp.use(Quasar, {
+  plugins: { Notify }
 });
 
-axios.interceptors.response.use(
-function (response){
-  requestsPending--;
-
-  if (requestsPending == 0){
-    Swal.close();
-  }
-  return response;
-},
-function (error) {
-requestsPending--;
-if(requestsPending == 0) {
-  Swal.close();
-}
-
-if (error.response.status == 401) {
- router .push('/')
-}
-
-return Promise.reject(error);
-});
-
-const pinia =createPinia()
-pinia.use(piniaPluginPersistedstate)
-const myApp = createApp(App)
-
-myApp.use(Quasar,  {
-  plugins: { Notify },
-  config: {
-    notify: {
-      position: 'top',
-      timeout: 2500,
-      textColor: 'white',
-      actions: [{ icon: 'close', color: 'white' }],
-      color:"red"
-    },
-  },
-})
-
-myApp.use(router)
-
-myApp.use(pinia)
-myApp.mount('#app')
+myApp.use(router);
+myApp.use(pinia);
+myApp.mount("#app");
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-
 import DjiT from "../components/DjiTelemetry.vue";
-import axios from "axios";
-
-
 
 const routes = [
-  { path: "DjiT", component: DjiT, meta }
+  { path: "/djit", component: DjiT } // Se corrigió el path (debe empezar con "/")
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-});
-
-router.beforeEach((to, from, next) => {
-  const useUsuario = useStoreUsuarios();
-  const userRole = useUsuario.role;
-
-  if (to.meta.roles && !to.meta.roles.includes(userRole)) {
-    next('/');
-  } else {
-    next();
-  }
 });
 
 export default router;
